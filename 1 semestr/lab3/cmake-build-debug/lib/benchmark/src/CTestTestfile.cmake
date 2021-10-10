e2acb6692f465acdae73faca9e00b752b89bbae6
AlexEsn <saschaesa@gmail.com>
2021-10-10 11:34:00 +0300

    Add new semester

--- 1 semestr/lab3/cmake-build-debug/lib/benchmark/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alexesn/CLionProjects/labs_inf_esis_alexander/lab3/lib/benchmark/src
-# Build directory: /home/alexesn/CLionProjects/labs_inf_esis_alexander/lab3/cmake-build-debug/lib/benchmark/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- lab1/start.sh
+++
@@ -1,9 +0,0 @@
-!/bin/bash
-
-mkdir cmake-build
-cd cmake-build
-cmake ..
-make
-cd ..
-mv cmake-build/lab1 .
-rm -rf cmake-build/
