b308a0f26c2278563ad3238273d7acbc704768c1
AlesEsn <saschaesa@gmail.com>
2021-10-21 18:56:13 +0300

    Update semester

--- 1 semester/lab3/cmake-build-debug/lib/benchmark/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alexesn/CLionProjects/labs_inf_esis_alexander/lab3/lib/benchmark
-# Build directory: /home/alexesn/CLionProjects/labs_inf_esis_alexander/lab3/cmake-build-debug/lib/benchmark
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
--- 1 semester/lab3/cmake-build-debug/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alexesn/CLionProjects/labs_inf_esis_alexander/lab3/lib/googletest/googlemock
-# Build directory: /home/alexesn/CLionProjects/labs_inf_esis_alexander/lab3/cmake-build-debug/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
